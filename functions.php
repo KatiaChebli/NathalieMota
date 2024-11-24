f42dcfdcbe62d791d95e2d5548ec419c6c651daa
Katia <Katiaa.chebli@gmail.cpm>
2024-11-24 13:49:00 +0100

    chargement du fichier js

--- functions.php
+++ functions.php
@@ -1,9 +1,26 @@
 <?php
-
-function register_my_menus() {
-    register_nav_menu('main-menu', __('Menu principal', 'text-domain'));
+// Ajout du styles 
+function nathaliemota_enqueue_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
-add_action('after_setup_theme', 'register_my_menus');
+add_action( 'wp_enqueue_scripts', 'nathaliemota_enqueue_scripts' );
+
+// Ajout des menus
+function nathaliemota_register_menus() {
+    register_nav_menus(
+        array(
+            'primary' => __( 'Menu Principal', 'nathaliemota' ),
+        )
+    );
+}
+add_action( 'after_setup_theme', 'nathaliemota_register_menus' );
+
+
+//charger fichier js
+function theme_enqueue_scripts() {
+    wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/js/scripts.js', [], false, true);
+}
+add_action('wp_enqueue_scripts', 'nathaliemota_enqueue_scripts');
 
 
 ?>
