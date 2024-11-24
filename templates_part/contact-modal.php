0caf7ec84635163ad2e1b9d3815e1a2d89b44084
Katia <Katiaa.chebli@gmail.cpm>
2024-11-24 14:00:22 +0100

    ajout dossier pour créer la modale contact

---
+++ templates_part/contact-modal.php
@@ -0,0 +1,9 @@
+<?php
+<div id="contact-modal" class="modal hidden">
+    <div class="modal-content">
+        <button class="close-modal">&times;</button>
+        <?php echo do_shortcode('[contact-form-7 id="ed125d2" title="Formulaire de contact 1"]'); ?>
+    </div>
+</div>
+
+?>
