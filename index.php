bbcf28d4a7f0c33c1e5a25e31dcd1480c14174f2
Katia <Katiaa.chebli@gmail.cpm>
2024-11-26 18:04:15 +0100

    rien

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php get_header(); ?>
 
 <main>
-    <h1><?php _e('Bienvenue sur mon site', 'text-domain'); ?></h1>
-    <?php
-        if (have_posts()) :
-            while (have_posts()) : the_post();
-                the_title('<h2>', '</h2>');
-                the_excerpt();
-            endwhile;
-        else :
-            _e('Aucun contenu disponible.', 'text-domain');
-        endif;
-    ?>
+    
 </main>
 
 <?php get_footer(); ?>
