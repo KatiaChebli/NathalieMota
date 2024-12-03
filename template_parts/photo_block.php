13bb856126f39e4c09e27eb21c55fea46344469d
Katia <Katiaa.chebli@gmail.cpm>
2024-12-03 11:49:38 +0100

    ajout code pour apparition des photos sur single page

--- template_parts/photo_block.php
+++ template_parts/photo_block.php
@@ -7,14 +7,16 @@ if (!isset($post)) $post = get_post();
 ?>
 
 <div class="related-photo-item">
-    <a href="<?php the_permalink(); ?>">
+    <!-- génère le lien vers la page de photo  -->
+    <a href="<?php the_permalink(); ?>"> 
+
         <?php
+        // affiche la miniature de la photo
         if (has_post_thumbnail()) {
             the_post_thumbnail('medium', ['style' => 'object-fit: cover; width: 100%; height: 30vh;']);
         } else {
             echo '<img src="' . esc_url(get_template_directory_uri() . '/assets/default-image.jpg') . '" alt="Default Image" />';
         }
         ?>
-        <h3><?php the_title(); ?></h3>
     </a>
 </div>
