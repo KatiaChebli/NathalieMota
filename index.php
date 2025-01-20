04d263dbe1cc594a93013aa84b6bbdc8aebd65fb
Katia <Katiaa.chebli@gmail.cpm>
2025-01-20 16:48:47 +0100

    redirection de la page single page

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
 <main>
-    <a href="<?php echo site_url('/single-photo.php');?>">Lien photo</a>
+
+<!-- hero -->
+<div id="hero"></div>
+<?php echo wp_list_pages()?>
+    <a href="<?php echo site_url('/template-single-photo.php');?>">Lien photo</a>
 </main>
 
 <?php get_footer(); ?>
